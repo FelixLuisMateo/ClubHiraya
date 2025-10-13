41958c9fcad03bb77e8128f613fb219ed14d5f47
FelixLuisMateo <mateo.felixluis@gmail.com>
2025-10-13 23:39:39 +0800

    Edit lang sa app.js

--- ClubHiraya/php/foods.php
+++ ClubHiraya/php/foods.php
@@ -2,15 +2,15 @@
 // Example food items for demonstration
 $foods = [
     // id, name, price, image, category
-    [ 'id' => 1, 'name' => 'Lechon Baka', 'price' => 420, 'image' => 'assets/lechon.jpg', 'category' => 'Main Course' ],
-    [ 'id' => 2, 'name' => 'Hoisin BBQ Pork Ribs', 'price' => 599, 'image' => 'assets/hoisin.jpg', 'category' => 'Main Course' ],
-    [ 'id' => 3, 'name' => 'Mango Habanero', 'price' => 439, 'image' => 'assets/mango.jpg', 'category' => 'Main Course' ],
+    [ 'id' => 1, 'name' => 'Lechon Baka', 'price' => 420, 'image' => 'assets/lechon baka.jpg', 'category' => 'Main Course' ],
+    [ 'id' => 2, 'name' => 'Hoisin BBQ Pork Ribs', 'price' => 599, 'image' => 'assets/hoisin BBQ Pork Ribs.jpg', 'category' => 'Main Course' ],
+    [ 'id' => 3, 'name' => 'Mango Habanero', 'price' => 439, 'image' => 'assets/mango habanero.jpg', 'category' => 'Main Course' ],
     [ 'id' => 4, 'name' => 'Smoked Carbonara', 'price' => 349, 'image' => 'assets/smoked_carbonara.jpg', 'category' => 'Main Course' ],
-    [ 'id' => 5, 'name' => 'Mozzarella Poppers', 'price' => 280, 'image' => 'assets/mozzarella.jpg', 'category' => 'Appetizer' ],
-    [ 'id' => 6, 'name' => 'Salmon Tare-Tare', 'price' => 379, 'image' => 'assets/salmon_tare.jpg', 'category' => 'Seafood Platter' ],
-    [ 'id' => 7, 'name' => 'Pork Sisig', 'price' => 280, 'image' => 'assets/sisig.jpg', 'category' => 'Main Course' ],
-    [ 'id' => 8, 'name' => 'Pepperoni Pizza', 'price' => 499, 'image' => 'assets/pizza.jpg', 'category' => 'Main Course' ],
+    [ 'id' => 5, 'name' => 'Mozzarella Poppers', 'price' => 280, 'image' => 'assets/mozzarella poppers.jpg', 'category' => 'Appetizer' ],
+    [ 'id' => 6, 'name' => 'Chili Lime Chicken Wings', 'price' => 379, 'image' => 'assets/Chickens Wings (Chili Lime).jpg', 'category' => 'Main Course' ],
+    [ 'id' => 8, 'name' => 'Pepperoni Pizza', 'price' => 499, 'image' => 'assets/pepperoni.jpg', 'category' => 'Main Course' ],
     [ 'id' => 9, 'name' => 'Seafood Cajun', 'price' => 1099, 'image' => 'assets/cajun.jpg', 'category' => 'Seafood Platter' ],
+    [ 'id' => 6, 'name' => 'Salmon Tare-Tare', 'price' => 379, 'image' => 'assets/salmon tare tare.jpg', 'category' => 'Seafood Platter' ],
     [ 'id' => 10, 'name' => 'French Fries', 'price' => 150, 'image' => 'assets/fries.jpg', 'category' => 'Side dish' ],
     [ 'id' => 11, 'name' => 'Buffalo Wings', 'price' => 420, 'image' => 'assets/buffalo.jpg', 'category' => 'Appetizer' ],
     [ 'id' => 12, 'name' => 'Iced Tea', 'price' => 99, 'image' => 'assets/iced_tea.jpg', 'category' => 'Drinks' ],
