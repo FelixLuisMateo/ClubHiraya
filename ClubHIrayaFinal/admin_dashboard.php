062de993fca7d86cfc2ff65952c9c209d13f659c
MagiToxic <nozomigoodshot@gmail.com>
2025-11-10 15:37:21 +0800

    meh

--- ClubHIrayaFinal/admin_dashboard.php
+++ ClubHIrayaFinal/admin_dashboard.php
@@ -36,28 +36,28 @@
     <!-- Sidebar -->
     <aside class="sidebar" role="complementary" aria-label="Sidebar">
         <div class="sidebar-header">
-            <img src="../ClubHirayaFinal/assets/foods/logos/logo1.png" alt="Club Hiraya logo" class="sidebar-header-img">
+            <img src="../ClubHirayaFinal/assets/logos/logo1.png" alt="Club Hiraya logo" class="sidebar-header-img">
         </div>
 
         <nav class="sidebar-menu" role="navigation" aria-label="Main menu">
             <a href="../ClubHirayaFinal /admin_dashboard.php" class="sidebar-btn active" aria-current="page">
-                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/foods/logos/home.png" alt="Home icon"></span>
+                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/logos/home.png" alt="Home icon"></span>
                 <span>Home</span>
             </a>
             <a href="../ClubHirayaFinal/tables/tables.php" class="sidebar-btn">
-                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/foods/logos/table.png" alt="Tables icon"></span>
+                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/logos/table.png" alt="Tables icon"></span>
                 <span>Cabins</span>
             </a>
             <a href="../ClubHirayaFinal/inventory/inventory.php" class="sidebar-btn">
-                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/foods/logos/inventory.png" alt="Inventory icon"></span>
+                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/logos/inventory.png" alt="Inventory icon"></span>
                 <span>Inventory</span>
             </a>
             <a href="php/../SalesReport/sales_report.php" class="sidebar-btn">
-                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/foods/logos/sales.png" alt="Sales report icon"></span>
+                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/logos/sales.png" alt="Sales report icon"></span>
                 <span>Sales Report</span>
             </a>
             <a href="../ClubHirayaFinal/settings/settings.php" class="sidebar-btn">
-                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/foods/logos/setting.png" alt="Settings icon"></span>
+                <span class="sidebar-icon"><img src="../ClubHirayaFinal/assets/logos/setting.png" alt="Settings icon"></span>
                 <span>Settings</span>
             </a>
         </nav>
@@ -78,7 +78,7 @@
             </div>
             <!-- Changed the Select Cabins control from a button to an anchor so it navigates to Cabins.php -->
             <a class="select-table-btn" role="link" href="../ClubHirayaFinal/Cabins/Cabins.php">
-                Select Cabins <span class="table-icon"><img src="../ClubHirayaFinal/assets/foods/logos/table.png" alt=""></span>
+                Select Cabins <span class="table-icon"><img src="../ClubHirayaFinal/assets/logos/table.png" alt=""></span>
             </a>
         </div>
 
