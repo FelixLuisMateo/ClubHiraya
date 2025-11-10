d5263a6da3ca2fe122dfe0e0207db96105dbdebb
MagiToxic <nozomigoodshot@gmail.com>
2025-11-10 16:23:25 +0800

    nageerror nanaman yung table ;-;

--- ClubHIrayaFinal/index.php
+++ ClubHIrayaFinal/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1440, initial-scale=1.0">
     <title>Club Hiraya</title>
-    <link rel="stylesheet" href="css/homepage.css">
+    <link rel="stylesheet" href="./css/homepage.css">
 </head>
 <body  class="welcome-page">
 
