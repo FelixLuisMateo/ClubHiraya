467f520cd0974c4d6e5d00685255dfe098058a17
MagiToxic <nozomigoodshot@gmail.com>
2025-11-10 13:09:29 +0800

    .

--- ClubHIrayaFinal/Cabins/Cabins.php
+++ ClubHIrayaFinal/Cabins/Cabins.php
@@ -17,13 +17,28 @@
       </div>
     </div>
 
-    <nav class="nav">
-      <a class="navbtn" href="../ClubHirayaFinal/admin_dashboard.php"><img src="home.png" alt="Home"> <span>Home</span></a>
-      <a class="navbtn" href="../ClubHirayaFinal/tables/tables.php" alt="Tables"> <span>Tables</span></a>
-      <a class="navbtn" href="../ClubHirayaFinal/inventory/inventory.php" alt="Inventory"> <span>Inventory</span></a>
-      <a class="navbtn" href="php/../SalesReport/sales_report.php"><img src="salesreport.png" alt="Sales"> <span>Sales Report</span></a>
-      <a class="navbtn" href="../ClubHirayaFinal/settings/settings.php" alt="Settings"> <span>Settings</span></a>
-    </nav>
+        <nav class="topbar-menu" class="sidebar-btn" aria-current="page">
+            <a href="../admin_dashboard.php" class="sidebar-btn" aria-current="page">
+                <span class="sidebar-icon"><img src="../assets/foods/logos/home.png" alt="Home icon"></span>
+                <span>Home</span>
+            </a>
+            <a href="../tables/tables.php" class="sidebar-btn active">
+                <span class="sidebar-icon"><img src="../assets/foods/logos/table.png" alt="Tables icon"></span>
+                <span>Cabins</span>
+            </a>
+            <a href="../inventory/inventory.php" class="sidebar-btn">
+                <span class="sidebar-icon"><img src="../assets/foods/logos/inventory.png" alt="Inventory icon"></span>
+                <span>Inventory</span>
+            </a>
+            <a href="../salesreport/../SalesReport/sales_report.php" class="sidebar-btn">
+                <span class="sidebar-icon"><img src="../assets/foods/logos/sales.png" alt="Sales report icon"></span>
+                <span>Sales Report</span>
+            </a>
+            <a href="../settings/settings.php" class="sidebar-btn">
+                <span class="sidebar-icon"><img src="../assets/foods/logos/setting.png" alt="Settings icon"></span>
+                <span>Settings</span>
+            </a>
+        </nav>
   </header>
 
   <main class="container">
